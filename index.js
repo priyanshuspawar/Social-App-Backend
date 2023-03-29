4cb155ca9f36b467b554fcc5c6c7a81bd5ecf602
priyanshuspawar <priyansusingh306@gmail.com>
2023-03-29 22:35:31 +0530

    multer file upload fix

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ connectDb();
 // for local storage
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, '/tmp/my-uploads')
+      cb(null, "public/assets")
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix)
+      cb(null, file.fieldname + '-' + uniqueSuffix+path.extname(file.originalname))
     }
   })
   
